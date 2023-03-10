637ef1cb2cdf2344d7bd8ca76d0f434b27f0b7e9
samnthorne <sam.n.thorne@gmail.com>
2023-03-10 14:19:02 -0800

    added function that joins data together

--- tests/tests.R
+++ tests/tests.R
@@ -1,6 +1,6 @@
 library(testthat)
 
-source("./R/majority_classifier_function.R")
+source("../R/majority_classifier_function.R")
 test_that("X-axis label is not the same!", {
           plot <- majority_classifier_vis_function(majority_classifier)
           expect_identical(plot$labels$x, 'Heart disease degree of severity')})
@@ -8,14 +8,14 @@ test_that("Y-axis label is not the same!", {
           plot <- majority_classifier_vis_function(majority_classifier)
           expect_identical(plot$labels$y, 'Percent of outcomes \n in training dataset')})
 
-source("./R/selection_forward_function.R")
+source("../R/selection_forward_function.R")
 plot <- forwardSelection(heart_training)
 test_that("The number of variables is not the same!", {
           expect_equivalent(max(plot$size), 5)})
 test_that("The results are not the same!", {
           expect_equivalent(min(plot$accuracy), 0.426789675597178)})
 
-source("./R/place_data.R")
+source("../R/place_data.R")
 # test that there are the right number of columns
 testthat::expect_equal(ncol(web_data('switzerland', url_switzerland)), 21)
 testthat::expect_equal(ncol(web_data('cleveland', url_cleveland)), 21)
