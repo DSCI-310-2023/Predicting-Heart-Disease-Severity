961108f8f8246b33396d6a4d242f1560912052c6
Daniel Lee <122393123+Dokkaebi10@users.noreply.github.com>
2023-03-25 11:43:04 -0700

    delete this script

--- R/04-build_classification_model.R
+++
@@ -1,7 +0,0 @@
-# This script builds a table of the classifcation model
-
-source('../R/classification_model.R')
-
-heart_data_accuracies <- classifier(heart_training)
-
-write_table(heart_data_accuracies, '../figures/classification_model.png')
