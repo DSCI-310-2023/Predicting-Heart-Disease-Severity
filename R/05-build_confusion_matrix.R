2aa7e105f08ecc487e5677dcc48c52f5cc4c248c
Daniel Lee <122393123+Dokkaebi10@users.noreply.github.com>
2023-03-25 11:43:31 -0700

    delete this script

--- R/05-build_confusion_matrix.R
+++
@@ -1,7 +0,0 @@
-# This script visualizes a confusion matrix
-
-source('../R/confusion_matrix.R')
-
-confuson_matrix('../data/modelling/predict_data.csv')
-
-ggsave('../R/confusion_matrix.png', confusion_matrix)
