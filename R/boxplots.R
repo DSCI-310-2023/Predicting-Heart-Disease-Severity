33f6a2bc9db7f10e781dc489012981c3a217888b
samnthorne <sam.n.thorne@gmail.com>
2023-03-26 17:29:09 -0700

    images are rendered inside jupyter book

--- R/boxplots.R
+++ R/boxplots.R
@@ -29,5 +29,5 @@ grid_boxplot <- function(data, yAxis, yLabel, titleLabel){
     geom_boxplot(color="#033291", fill="#b0c8f7", alpha=0.2) +
     labs(x="Degree of heart disease", y=yLabel) +
     ggtitle(titleLabel) +
-    theme(text = element_text(size = 24))
+    theme(text = element_text(size = 12))
 }
